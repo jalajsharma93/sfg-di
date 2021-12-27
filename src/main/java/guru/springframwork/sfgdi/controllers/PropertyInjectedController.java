caaf6ef598c51e7ceaacf4ed65d7c4a3738013cc
jalajsharma93 <jalajsharma92@gmail.com>
2021-12-27 04:42:50 -0500

    Di through property injector

---
+++ src/main/java/guru/springframwork/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,12 @@
+package guru.springframwork.sfgdi.controllers;
+
+import guru.springframwork.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    public GreetingService greetingService;
+
+    public  String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}

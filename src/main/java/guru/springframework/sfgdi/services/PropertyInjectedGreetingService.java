0626083f1b1a989165a01c3c9fd775cc4d311159
jalajsharma93 <jalajsharma92@gmail.com>
2022-01-09 16:43:42 -0500

    Configuration changes as using Java Config

--- src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -3,6 +3,6 @@ package guru.springframework.sfgdi.services;
 public class PropertyInjectedGreetingService implements GreetingService{
     @Override
     public String sayGreeting(){
-        return "Hello from Property Injected service";
+        return "Hello from Property Injected service:Java Config EX";
     }
 }
